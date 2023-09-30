881755a6c9c712061371cb79c229d897e9358646
Ali-Maghari <aliplaywork@gmail.com>
2023-10-01 00:38:31 +0300

    Lecture 1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,72 @@
+fun main(args: Array<String>) {
+
+    // datatype variableName = value;
+//    var name: String? = null
+//    var age: Int? = null
+
+//    if (1 == 1) {
+//        println("1 is equal to 1")
+//    } else {
+//        println("1 is not equal to 1")
+//    }
+
+//    val age = (1..100).random()
+//    when (age) {
+//        20 -> {
+//            println("You are 20 years old")
+//        }
+//        30 -> {
+//            println("You are 30 years old")
+//        }
+//        else -> {
+//            println("You are not 20 or 30 years old")
+//        }
+//    }
+
+    // list
+//    val subNames = arrayListOf("Ahmed", "Bassam")
+//    val arrNames: ArrayList<String> = ArrayList()
+//    arrNames.add("John")
+//    arrNames.addAll(subNames)
+//    println(arrNames)
+
+    // set
+//    val namesSet: MutableSet<String> = mutableSetOf()
+//    namesSet.add("Ahmed")
+//    namesSet.add("Ahmed")
+//    namesSet.add("Monther")
+//    namesSet.add("Ali")
+//    println(namesSet)
+
+    // map
+//    val namesMap: MutableMap<Int, Any> = mutableMapOf()
+//    namesMap[1] = "Ahmed"
+//    namesMap[2] = true
+//    println(namesMap)
+
+    // casting
+//    val age = 20
+//    val ageString = age.toString()
+
+    // loops
+//    for (i in 10 downTo 1) {
+//        println(i)
+//    }
+//    val subNames = arrayListOf("Ahmed", "Bassam")
+//    for (name in subNames) {
+//        println(name)
+//    }
+//    subNames.forEach { name ->
+//        println(name)
+//    }
+//    subNames.forEach {
+//
+//    }
+
+    // scanner
+//    println("Enter your age :")
+//    var age = readln()?.toInt()
+//    var currentYear = 2023
+//    println("Your age is $age years old, and the current year is $currentYear, and the sum ${ currentYear + (age ?: 0) }")
+
+}
